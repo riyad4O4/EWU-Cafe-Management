788b0a4baba3cc9de50f4c4ed177893a2567e7b4
Riyad khan <mdriyadkhan4472@gmail.com>
2026-08-03 20:01:02 +0600

    Added funtions name in the file

---
+++ payment.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "payment.h"
+
+int cash_payment(int a);
+{
+
+}
+int card_payment();
+{
+
+}
+int discount();
+{
+
+}
+int monthly_sales();
+{
+
+}
+int daily_sales();
+{
+
+}
