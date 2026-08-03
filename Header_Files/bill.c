3113f7e289357b2321afcb663395d3e61973f13e
AZRAF FAHIM <azraffahim96@gmail.com>
2026-08-03 21:56:03 +0600

    created header file for bill

---
+++ Header_Files/bill.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include "bill.h"
+
+int place_order()
+{
+    // Implementation for placing an order
+}
+
+int select_food()
+{
+    // Implementation for selecting food
+}
+
+int food_quantity()
+{
+    // Implementation for setting food quantity
+}
+
+int generate_bill()
+{
+    // Implementation for generating bill
+}
+
+int calculate_total()
+{
+    // Implementation for calculating total
+}
+
+int print_receipt()
+{
+    // Implementation for printing receipt
+}
