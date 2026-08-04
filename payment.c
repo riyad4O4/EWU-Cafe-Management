54011ed24ab2d606bcba3a4598e2dbaff6eaa259
Riyad khan <mdriyadkhan4472@gmail.com>
2026-08-04 23:11:59 +0600

    Completed cash_payment, card_payment and discount function's logic

--- payment.c
+++ payment.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include "payment.h"
 
-int cash_payment(int a)
+int cash_payment(float total_bill)
 {
-
+    int cash;
+    printf("\nCASH : ");
+    scanf("%d",&cash );
+    printf("Change %d",cash-total_bill);
+    printf("Order Paid Successfully");
 }
-int card_payment()
+int card_payment(float toal_bill)
 {
-
+    printf("Total Bill %d",toal_bill);
+    printf("Enter Pin : ");
+    printf("Enter amount to pay : ");
+    printf("Order Paid Successfully");
 }
-int discount()
+int discount(float total_price)
 {
-
+    if (total_price>1000)
+    {
+        total_price=total_price-(total_price*0.05);
+        printf("Price after 5\% discount = %d",total_price);
+    }
+    
 }
 int monthly_sales()
 {
-
+    
 }
 int daily_sales()
 {
