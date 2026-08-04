55dc2d1b10657748224fd4148d364cc8887e6e5b
Riyad khan <mdriyadkhan4472@gmail.com>
2026-08-04 23:44:10 +0600

    added comments and improved card_payment bugfix: changed input format from %d to %f

--- payment.c
+++ payment.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
 #include "payment.h"
 
-int cash_payment(float total_bill)
+int cash_payment(float total_bill) // takes total bill
 {
     int cash;
     printf("\nCASH : ");
-    scanf("%d", &cash);
-    printf("Change %d", cash - total_bill);
-    printf("Order Paid Successfully");
+    scanf("%f", &cash);                       // takes customers cash amount
+    printf("Change %f\n", cash - total_bill); // calculates change
+    printf("Order Paid Successfully\n");
     return 0;
 }
-int card_payment(float toal_bill)
+int card_payment(float total_bill)// takes total bill
 {
-    printf("Total Bill %d", toal_bill);
-    printf("Enter Pin : ");
-    printf("Enter amount to pay : ");
-    printf("Order Paid Successfully");
-    return 0;
-}
-int discount(float total_price)
-{
-    if (total_price > 1000)
+    int temp, amount;
+    printf("Total Bill %f\n", total_bill);
+    printf("Enter amount to pay : \n");
+    scanf("%f", &amount);
+    while (1) // infinite loop until payments success
     {
-        total_price = total_price - (total_price * 0.05);
-        printf("Price after 5\% discount = %d", total_price);
+
+        if (amount == total_bill)
+        {
+            printf("Enter Pin : \n");
+            scanf("%f", &temp);
+            printf("Order Paid Successfully\n");
+            break;//after payment successful it breaks the loop otherwise loop will be infinite
+        }
+        else
+        {
+            printf("Payment Failed !!!\n");
+            printf("Invalid amount \n");
+        }
+    }
+
+    return 0;
+}//will add feature like maximum 3 tries than cancel the order 
+int discount(float total_bill)// takes total bill
+{
+    if (total_bill > 1000)//if bill is greater than 1000 added 5% discount
+    {
+        total_bill = total_bill - (total_bill * 0.05);//5% discount added
+        printf("\nPrice after 5\% discount = %f\n", total_bill);
     }
 
     return 0;
