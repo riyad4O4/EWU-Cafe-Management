0122fb051ec48ff57b95307728c39f82c1b86054
Riyad khan <mdriyadkhan4472@gmail.com>
2026-08-04 23:14:06 +0600

    added return 0 and formatted the code

--- payment.c
+++ payment.c
@@ -5,31 +5,32 @@ int cash_payment(float total_bill)
 {
     int cash;
     printf("\nCASH : ");
-    scanf("%d",&cash );
-    printf("Change %d",cash-total_bill);
+    scanf("%d", &cash);
+    printf("Change %d", cash - total_bill);
     printf("Order Paid Successfully");
+    return 0;
 }
 int card_payment(float toal_bill)
 {
-    printf("Total Bill %d",toal_bill);
+    printf("Total Bill %d", toal_bill);
     printf("Enter Pin : ");
     printf("Enter amount to pay : ");
     printf("Order Paid Successfully");
+    return 0;
 }
 int discount(float total_price)
 {
-    if (total_price>1000)
+    if (total_price > 1000)
     {
-        total_price=total_price-(total_price*0.05);
-        printf("Price after 5\% discount = %d",total_price);
+        total_price = total_price - (total_price * 0.05);
+        printf("Price after 5\% discount = %d", total_price);
     }
-    
+
+    return 0;
 }
 int monthly_sales()
 {
-    
 }
 int daily_sales()
 {
-
 }
