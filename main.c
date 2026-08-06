337b4f9848672d3dd7453d781e815ae5cb60aca2
Riyad khan <mdriyadkhan4472@gmail.com>
2026-08-06 23:28:49 +0600

    Bugfix

---
+++ main.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include "main_menu.h"
+int main() 
+{   
+    
+    int choice;
+    printf("Main Menu:\n");
+    printf("1. Faculty\n");
+    printf("2. Student\n");
+    printf("3. Admin\n");
+    printf("4. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    if(choice == 1) 
+    {
+        account();
+    } 
+    else if(choice == 2) 
+    {
+        account();
+    } 
+    else if(choice == 3) 
+    {
+        account();
+    } 
+    else if(choice == 4) 
+    {
+        account();
+    } 
+    else 
+    {
+        printf("Invalid choice. Please try again.\n");
+    }
+    
+    return main();
+}
--- main_menu.c
+++ main_menu.c
@@ -1,39 +1,9 @@
-#include<stdio.h>
-#include "menu.h"
-int main() 
-{   
-    
-    int choice;
-    printf("Main Menu:\n");
-    printf("1. Faculty\n");
-    printf("2. Student\n");
-    printf("3. Admin\n");
-    printf("4. Exit\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
+#include <stdio.h>
+#include "main_menu.h"
 
-    if(choice == 1) 
-    {
-        account();
-    } 
-    else if(choice == 2) 
-    {
-        account();
-    } 
-    else if(choice == 3) 
-    {
-        account();
-    } 
-    else if(choice == 4) 
-    {
-        account();
-    } 
-    else 
-    {
-        printf("Invalid choice. Please try again.\n");
-    }
-    
-    return main();
+int menu()
+{
+    //code
 }
 
 
