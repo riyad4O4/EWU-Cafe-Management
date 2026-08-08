2fd28b6fdfc7ab9417ac7498459488fb179c316a
pur-na-ta <chandrima.purnata012@gmail.com>
2026-08-09 00:30:58 +0600

    Added comments

--- Account.c
+++ Account.c
@@ -6,36 +6,37 @@ struct Account
 {
     char id[20];
     char email[40];
-    char phone[15];
+    char phone[15];  
     char password[15];
 };
 
+//Function for signing up
 int sign_up()
 {
     create_account();
 }
 
+//Function to create a new account
 int create_account()
 {
-struct Account user;
-struct Account temp;
+struct Account user;  //Stores new user's information
+struct Account temp;  //Temporary stores existing account information
 FILE *fp;
-fp=fopen("accounts.txt", "a+");
-if(fp==NULL)
+fp=fopen("accounts.txt", "a+");  //Open accounts.txt in append and reads
+if(fp==NULL)  //Check if the files could not be opened
 {
     printf("File Error!\n");
 }
 printf("\n----CREATE ACCOUNT----\n");
+//Take input
 scanf("%s", user.id);
-
-//Check if ID already exists
-rewind(fp);
-while(fscanf(fp, "%s %s %s %s", temp.id, temp.email, temp.phone, temp.password)!= EOF)
+rewind(fp); //Check if ID already registered
+while(fscanf(fp, "%s %s %s %s", temp.id, temp.email, temp.phone, temp.password)!= EOF) //Read registered accounts from the file
 {
-    if(strcmp(user.id, temp.id)== 0)
+    if(strcmp(user.id, temp.id)== 0)  //Compare entered ID with registered ones
     {
         printf("This Student ID already exists!\n");
-            fclose(fp);
+            fclose(fp);  //Close the file
     }
 }
     printf("Enter Email: ");
@@ -44,7 +45,7 @@ while(fscanf(fp, "%s %s %s %s", temp.id, temp.email, temp.phone, temp.password)!
     printf("Enter Phone Number: ");
     scanf("%s", user.phone);
 
-int valid;
+int valid;  //Asking for a valid password
 
     do
     {
@@ -53,7 +54,7 @@ int valid;
 
         valid = valid_password(user.password);
 
-        if(valid == 0)
+        if(valid == 0)  //If password is invalid, show the imposed requirements
         {
             printf("\n Invalid Password! Try Again \n");
             printf("Password must contain:- At least 8 characters\n");
@@ -69,24 +70,25 @@ int valid;
             user.id,
             user.email,
             user.phone,
-            user.password);
+            user.password);  //Save new account's information into the file
 
-    fclose(fp);
+    fclose(fp);  //Close the file
 
     printf("\n Account Created Successfully! \n");
 }
-int valid_password(char password[])
+int valid_password(char password[]) //Function to check the validity of password
 {
+    //Flags to check imposed reqirements for password
     int upper = 0;
     int lower = 0;
     int number = 0;
     int special = 0;
 
-    if(strlen(password) < 8)
+    if(strlen(password) < 8)  //The length of the password which should be constructed with at least 8 characters
     {
         return 0;
     }
-
+//Check each character of the password
     for(int i = 0; password[i] != '\0'; i++)
     {
         if(password[i] >= 'A' && password[i] <= 'Z')
@@ -109,26 +111,26 @@ int valid_password(char password[])
 
     if(upper == 1 && lower == 1 && number == 1 && special == 1)
     {
-        return 1;
+        return 1; //Password fulfills given requirements
     }
 
-    return 0;
+    return 0; //Password is invalid
 }
 
-int log_in()
+int log_in()  //Function for user login
 {
 struct Account user;
 
     char id[20];
     char password[15];
 
-    int found = 0;
+    int found = 0;  //Checks whether login information is correct
 
     FILE *fp;
 
-    fp = fopen("accounts.txt", "r");
+    fp = fopen("accounts.txt", "r");  //Open and reads accounts file
 
-    if(fp == NULL)
+    if(fp == NULL)  //Checks if the file exists
     {
         printf("No Account Found!\n");
         return;
@@ -146,19 +148,19 @@ struct Account user;
                  user.id,
                  user.email,
                  user.phone,
-                 user.password) != EOF)
+                 user.password) != EOF) //Reads each and every from the accounts file
     {
         if(strcmp(id, user.id) == 0 &&
-           strcmp(password, user.password) == 0)
+           strcmp(password, user.password) == 0)  //Compares entered ID and password with stored infos
         {
-            found = 1;
-            break;
+            found = 1;  //Login info is correct
+            break;  //Stop searching
         }
     }
 
     fclose(fp);
 
-    if(found == 1)
+    if(found == 1)  //Display login result
     {
         printf("\nLogin Successful!\n");
     }
@@ -167,7 +169,7 @@ struct Account user;
         printf("\nWrong Student ID or Password!\n");
     }
 }
-
+//Function to change user's password
 int change_password()
 {
     struct Account user;
@@ -176,23 +178,23 @@ int change_password()
     char oldpass[15];
     char newpass[15];
 
-    int found = 0;
+    int found = 0;  //Checks whecther the accound was found
     int valid;
 
     FILE *fp;
     FILE *temp;
 
-    fp = fopen("accounts.txt", "r");
+    fp = fopen("accounts.txt", "r");  //Open the original account file
 
-    if(fp == NULL)
+    if(fp == NULL)  //Checks if the file not exists
     {
         printf("No Account Found!\n");
         return;
     }
 
-    temp = fopen("temp.txt", "w");
+    temp = fopen("temp.txt", "w");  //Create a temporary file
 
-    if(temp == NULL)
+    if(temp == NULL) //Checks if the temporary file could not be created
     {
         printf("File Error!\n");
         fclose(fp);
@@ -206,19 +208,20 @@ printf("\n CHANGE PASSWORD: \n");
     printf("Old Password: ");
     scanf("%s", oldpass);
 
+//Keep asking for a new password until it is valid
     do
     {
         printf("New Password: ");
         scanf("%s", newpass);
 
-        if(strcmp(oldpass, newpass) == 0)
+        if(strcmp(oldpass, newpass) == 0) //New password and old password cannot be same
         {
             printf("New password cannot be the same as old password.\n");
             valid = 0;
         }
         else
         {
-            valid = valid_password(newpass);
+            valid = valid_password(newpass);  //Check whether the new password fulfills requirement
 
             if(valid == 0)
             {
@@ -233,16 +236,16 @@ while(fscanf(fp, "%s %s %s %s \n",
                  user.id,
                  user.email,
                  user.phone,
-                 user.password) != EOF)
+                 user.password) != EOF)  //Reads all account from the original file
     {
         if(strcmp(id, user.id) == 0 &&
-           strcmp(oldpass, user.password) == 0)
+           strcmp(oldpass, user.password) == 0)  //Checks whether it's the same account who's password should be changed
         {
             fprintf(temp, "%s %s %s %s\n",
                     user.id,
                     user.email,
                     user.phone,
-                    newpass);
+                    newpass);  //Register the account with new password
 
             found = 1;
         }
@@ -252,17 +255,18 @@ while(fscanf(fp, "%s %s %s %s \n",
                     user.id,
                     user.email,
                     user.phone,
-                    user.password);
+                    user.password);  //Copy other accounts without changing them
         }
     }
 
+    //Close both files
     fclose(fp);
     fclose(temp);
 
-    remove("accounts.txt");
-    rename("temp.txt", "accounts.txt");
+    remove("accounts.txt");  //Delete the old accounts file
+    rename("temp.txt", "accounts.txt");  //Rename the temporary file as accounts.txt
 
-    if(found == 1)
+    if(found == 1)  //Display the result
     {
         printf("\nPassword Changed Successfully!\n");
     }
@@ -272,12 +276,12 @@ while(fscanf(fp, "%s %s %s %s \n",
     }
 }
 
-int log_out()
+int log_out()  //Function to log out
 {
 printf("\n Logged out successfully! \n");
 }
 
-int admin_login()
+int admin_login()  //Funtion for admin login
 {
     char password[15];
     printf("\n ADMIN LOGIN \n");
@@ -285,7 +289,7 @@ int admin_login()
     printf("Password: ");
     scanf("%s", password);
 
-    if(strcmp(password, "1234") == 0)
+    if(strcmp(password, "1234") == 0)  //Checks if the given password is correct
     {
         printf("\n Admin Login Successful! \n");
     }
