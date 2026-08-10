c662f859f8fcf7abfb86581765290d35b2ef0eb3
Riyad khan <mdriyadkhan4472@gmail.com>
2026-08-10 20:16:36 +0600

    Update payment.c

--- payment.c
+++ payment.c
@@ -3,44 +3,58 @@
 
 int cash_payment(float total_bill) // takes total bill
 {
-    int cash;
+    float cash;
     printf("\nCASH : ");
-    scanf("%f", &cash);                       // takes customers cash amount
+    scanf("%f", &cash); // takes customers cash amount
+    if (cash < total_bill)
+    {
+        printf("Insufficient cash\n");
+        return cash_payment(total_bill); // if customer pays less than total bill it starts again
+    }
     printf("Change %f\n", cash - total_bill); // calculates change
     printf("Order Paid Successfully\n");
     return 0;
 }
-int card_payment(float total_bill)// takes total bill
+int card_payment(float total_bill) // takes total bill
 {
-    int temp, amount;
-    printf("Total Bill %f\n", total_bill);
-    printf("Enter amount to pay : \n");
-    scanf("%f", &amount);
+    float temp, amount;
+    int count = 0;
     while (1) // infinite loop until payments success
     {
+        printf("Total Bill %f\n", total_bill);
+        printf("Enter amount to pay : \n");
+        scanf("%f", &amount);
 
         if (amount == total_bill)
         {
             printf("Enter Pin : \n");
-            scanf("%f", &temp);
+            scanf("%f", &temp); // sudo pin input
             printf("Order Paid Successfully\n");
-            break;//after payment successful it breaks the loop otherwise loop will be infinite
+            return 0;
         }
         else
         {
             printf("Payment Failed !!!\n");
             printf("Invalid amount \n");
+            count++;
+        }
+
+        if (count == 3)
+        {
+            printf("Order cancelled\n");
+            printf("Pay Via Cash\n");
+            cash_payment(total_bill);
+            break;
         }
     }
-
     return 0;
-}//will add feature like maximum 3 tries than cancel the order 
-int discount(float total_bill)// takes total bill
+}
+int discount(float total_bill) // takes total bill
 {
-    if (total_bill > 1000)//if bill is greater than 1000 added 5% discount
+    if (total_bill > 1000) // if bill is greater than 1000 added 5% discount
     {
-        total_bill = total_bill - (total_bill * 0.05);//5% discount added
-        printf("\nPrice after 5\% discount = %f\n", total_bill);
+        total_bill = total_bill - (total_bill * 0.05); // 5% discount added
+        printf("\nPrice after 5%% discount = %f\n", total_bill);
     }
 
     return 0;
