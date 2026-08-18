8a98ba4fa6aff1f8395e3e3cd36845369358443a
Mahathir Nahiyan Hasan <nahiyanpersonal14@gmail.com>
2026-08-18 17:17:43 +0600

    Included all header file

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 // ID: 2026-2-60-543
-#include <stdio.h>
-#include "main_menu.h"
+>  #include <stdio.h>
+ #include "main_menu.h"
 #include "Account.h"
 #include "admin_menu.h"
 #include "customer.h"
 #include "main_menu.h"
-#include "Payment.h"
-
 int main()
 {
 
