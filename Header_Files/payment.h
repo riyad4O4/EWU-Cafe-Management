97e335838c542b343139fb825cc105af3be59148
Riyad khan <mdriyadkhan4472@gmail.com>
2026-08-18 16:16:21 +0600

    Made make file and fixes some bugs

--- Header_Files/payment.h
+++ Header_Files/payment.h
@@ -2,9 +2,9 @@
 #ifndef PAYMENT_H
 #define PAYMENT_H
 
-int cash_payment();
-int card_payment();
-int discount();
+int cash_payment(float total_bill);
+int card_payment(float total_bill);
+int discount(float total_bill);
 int monthly_sales();
 int daily_sales();
 
